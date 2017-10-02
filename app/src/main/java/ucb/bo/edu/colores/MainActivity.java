0a88741d38b19063283488cceb625b365d40fc26
Roberto Carlos Callisaya Mamani <calyr_software@hotmail.com>
2017-10-01 22:36:02 -0400

    ActionBar

--- app/src/main/java/ucb/bo/edu/colores/MainActivity.java
+++ app/src/main/java/ucb/bo/edu/colores/MainActivity.java
@@ -3,6 +3,7 @@ package ucb.bo.edu.colores;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -11,10 +12,21 @@ public class MainActivity extends AppCompatActivity {
 
     private View layoutPrincipal;
     private Button btnWhite;
+    private Toolbar mToolBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.mToolBar = (Toolbar) findViewById(R.id.toolbar);
+
+        if (this.mToolBar != null) {
+            setSupportActionBar(this.mToolBar);
+        }
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         this.layoutPrincipal = (LinearLayout) findViewById(R.id.layoutPrincipal);
         this.btnWhite = (Button) findViewById(R.id.btnWhite);
 
